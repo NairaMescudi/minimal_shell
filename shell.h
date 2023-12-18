c3479550bd5e9c2765d4ed09aa509062ac607100
NairaMescudi <tdaisioso@gmail.com>
2023-12-18 23:48:21 +0100

    Adding the header file changes for the project

---
+++ shell.h
@@ -0,0 +1,14 @@
+#ifndef SHELL_H
+#define SHELL_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <ctype.h>
+
+
+
+#endif
