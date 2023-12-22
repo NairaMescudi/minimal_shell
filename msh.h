2c2c0d1d7192272c0a96508884f97220bef599d2
NairaMescudi <tdaisioso@gmail.com>
2023-12-22 02:16:26 +0100

    Changes to msh.jh

--- msh.h
+++ msh.h
@@ -9,35 +9,11 @@
 #include <unistd.h>
 #include <ctype.h>
 #include <errno.h>
-#define LARGE_BUF_SIZE 1024
-#define SMALL_BUF_SIZE 120
 
-/* Shell custom data type - Linked List */
-typedef msh
-{
-	int exit_code;
-	const char *user_input;
-	const char *command;
-	alias_t *aliases;
+/* Checks for signal interrupts from keyboard */
+void sigint_handler(int signum);
 
-	
-} msh_t;
-
-/* Alias List custom data type - Linked List */
-typedef alias
-{
-	alias_node *head;
-	alias_node *tail;
-	size_t size;
-} alias_t;
-
-/* Alias Node custom data type - Struct */
-typedef aliasNode
-{
-	const char *name;
-	const char *value;
-} alias_node;
-
-
+/* Display the shell prompt */
+void display_prompt(void);
 
 #endif
